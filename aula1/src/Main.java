b055c598df75d80ba2bacde075ab912754fa243b
Marcio Taier <128880383+MTaier@users.noreply.github.com>
2024-02-22 20:52:11 -0300

    Add files via upload

---
+++ aula1/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(fatorial(n));
+        mostraPares(n);
+    }
+    public static int fatorial(int n){
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * fatorial(n - 1);
+    };
+
+    public static void mostraPares(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
